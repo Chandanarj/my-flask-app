5e1f9ea3fcec532c323760010b99cfe9cbd1bfbf
chandana naidu <chandanarj@gmail.com>
2024-06-04 12:39:30 +0530

    initial commit

---
+++ app/init.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@db:5432/mydatabase'
+db = SQLAlchemy(app)
+
+from app import routes
+
